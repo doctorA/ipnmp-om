603ddc3cebc1b1ed35e2cea2b6b29a683dc47d85
dejan.volk <dejan.volk@867793c8-2e47-11de-8ffe-b512714d229c>
2009-04-25 16:29:25 +0000

    Dodan class Medicinski_pripomočki.cs

---
+++ development/IPNMP/Medicinski_pripomočki.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPNMP
+{
+    public class Medicinski_pripomočki
+    {
+        public string Naziv
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Kategorija
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void VrniVseMedPrip()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
